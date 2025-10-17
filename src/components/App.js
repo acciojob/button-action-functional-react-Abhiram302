94217a9cc08ba8defeb75d8bfc5219f4e121a67c
Abhiram302 <abhiramburra3@gmail.com>
2025-10-17 11:30:34 +0530

    again

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [para,setPara] = useState("")
+  const [para,setPara] = useState(false)
   return (
     <div id="main">
       <button id = "click" onClick={()=>{
-        setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.")
+        setPara(true)
       }}>Click</button>
 
-      <p id="para">{para}</p>
+      {/* <p id="para">{para}</p> */}
+      {para && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
